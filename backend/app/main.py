601f354b2f0276b562ca862ce27a2221ae25c0ff
camper3d <camper3d@gmail.com>
2025-06-01 12:42:48 +0300

    зависимости, применение camel_case, delete_task

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,10 +5,10 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from models import Task, CreateTask
 from db_models import TaskORM
-from database import SessionLocal, engine
+from database import SessionLocal, engine, Base
 from utils import to_camel_case
-from database import Base
-Base.metadata.create_all(bind=engine)
+
+Base.metadata.create_all(bind=engine) # создаём таблицы
 
 app = FastAPI()
 
@@ -31,31 +31,38 @@ def get_db():
 
 
 @app.get("/api/tasks", response_model=List[Task])
-def get_tasks():
-    tasks = [
-        Task(id=1, title=to_camel_case('Получить удовольствие от процесса'), is_completed=True),
-        Task(id=2, title=to_camel_case('Пофиксить баги'), is_completed=True),
-        Task(id=3, title=to_camel_case('Написать фронтенд'), is_completed=False)
-    ]
+def get_tasks(db: Session = Depends(get_db)):
+    tasks = db.query(TaskORM).all() # возвращаем все задачи
     return tasks
 
 
 @app.post('/api/tasks', response_model=Task)
 def create_task(task: CreateTask, db: Session = Depends(get_db)):
     try:
-        db_task = TaskORM(title=task.title, is_completed=task.is_completed) # создаём задачу
+        camel_title = to_camel_case(task.title)
+        db_task = TaskORM(title=camel_title, completed=task.completed) # создаём задачу
         db.add(db_task) # добавляем задачу в базу
         db.commit() # сохраняем задачу в базу
         db.refresh(db_task) # обновляем объект из базы данных
 
-        return Task(id=db_task.id, title=to_camel_case(db_task.title), is_completed=db_task.is_completed)
+        return Task(id=db_task.id, title=db_task.title, completed=db_task.completed)
     except IntegrityError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Такая задача уже есть")
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Ошибка базы данных')
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Ошибка сервера')
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@app.delete('/api/tasks/{task_id}')
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(TaskORM).filter(TaskORM.id == task_id).first() # поиск нужной задачи
+    if not task:
+        raise HTTPException(status_code=404, detail='Нет задачи')
+    db.delete(task) # удаляем задачу
+    db.commit() # комитим изменение
+    return {'msg': 'задача удалена'}
 
 
